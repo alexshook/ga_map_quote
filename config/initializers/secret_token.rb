f473d88d9750e0a886ec46a0005efbd0b85c8cf6
alexshook <alexandra.shook@gmail.com>
2014-03-06 13:27:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GaMapQuote::Application.config.secret_key_base = '6af3b71ba1ed3a0292e6f5a2ae345f8f20a3da9e510de1017bd68971775c6eb7bad9b30e32c7b4277c2b848755a8bbcd638b9e23b36c65850b9fded0c781c9ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GaMapQuote::Application.config.session_store :cookie_store, key: '_ga_map_quote_session'
