a964dad79f015f98ef39b72db35924911143c966
alexshook <alexandra.shook@gmail.com>
2014-03-17 23:33:10 -0400

    committing before branching

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable
   # :registerable has been removed to make the site by invitation only
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :invitable
 end
