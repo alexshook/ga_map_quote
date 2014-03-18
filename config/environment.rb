69f27f2f9085019a8d0ac3b40ea87ab60cc8786b
alexshook <alexandra.shook@gmail.com>
2014-03-18 00:00:42 -0400

    tried to fix with ENV and following heroku documentation but console won't even start - /Users/alexshook/code/ga_map_quote/config/environment.rb:2:in config' for main:Object (NameError)

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 GaMapQuote::Application.initialize!
 
-# #from heroku doc
-# config.gem 'redis'
+#from heroku doc
+config.gem 'redis'
