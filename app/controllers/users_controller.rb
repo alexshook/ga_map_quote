8aff9da062d8ec09db114542c462abf1652f37da
alexshook <alexandra.shook@gmail.com>
2014-03-18 23:44:57 -0400

    more styling and routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,41 @@ class UsersController < ApplicationController
 
   before_filter :authenticate!, except: :index
 
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.create
+  end
+
+  def create
+    user = User.create user_params
+  end
+
+  def update
+    @user = User.find params[:id]
+    user.update_attributes!(user_params)
+    user = User.update user_params
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
+
+  def edit
+    @user = User.find params[:id]
+    user = User.update user_params
+  end
+
   private
 
   def authenticate!
   redirect_to root_path unless user_signed_in? or admin_signed_in?
   end
 
-end
+  def user_params
+    params.require(:user).permit(:email)
+  end
 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ GaMapQuote::Application.routes.draw do
   devise_for :admins
   devise_for :users
 
+  resources :users, only: [ :edit, :update ]
+
   resources :quotes, only: [ :index, :new, :create, :show, :edit, :update, :destroy ]
 
 end
